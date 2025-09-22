04669ef86e951e491c5e5c9888e4d16937ef48b4
JakeCasserly <45823946+JakeCasserly@users.noreply.github.com>
2025-09-22 22:46:07 +0100

    Create 33. Search in Rotated Sorted Array.cpp

---
+++ 33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0;
+        int r = n - 1;
+    
+        while (l < r) {
+            int m = (l + r) / 2;
+    
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+    
+        int minIndex = l;
+    
+        if (minIndex == 0) {
+            l = 0;
+            r = n - 1;
+        } else if (target >= nums[0] && target <= nums[minIndex - 1]) {
+            l = 0;
+            r = minIndex - 1;
+        } else {
+            l = minIndex;
+            r = n - 1;
+        }
+
+        while (l <= r) {
+            int middle = floor((l+r)/2);
+            if (nums[middle] == target) {
+                return middle;
+            }
+            else if (nums[middle] < target) {
+                l = middle + 1;
+            }
+            else {
+                r = middle - 1;
+            }
+        }
+        return -1;
+    }
+};
